25eb9a71a1fe34d9b17c1acd452034fac1414443
Fantine R <56721893+fantinerudent@users.noreply.github.com>
2020-06-20 23:36:33 +0200

    server

--- server.js
+++ server.js
@@ -19,11 +19,18 @@ const session = {
   resave: false,
 };
 
+const publicPath = path.join(__dirname, '..', 'public');
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "routes")));
-app.use('./public/uploads', express.static("uploads"))
+// app.use(express.static(path.join(__dirname, "routes")));
+// app.use('./public/uploads', express.static("uploads"))
 
 app.use(expressSession(session));
 
